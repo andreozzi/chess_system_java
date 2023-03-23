ff7660e133623a22c57af2ba9a01d0f2bec75dc4
Victor Andreozzi <victor.andreozzi.dev@outlook.com>
2023-03-23 15:25:59 -0300

    First Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world1");
+        Position pos = new Position(3,2);
+        System.out.println(pos);
     }
 }
